f47c82426817320da81ae5b6b7d2e32bdabe837a
ballin-23 <ballincode@gmail.com>
2023-08-15 00:38:22 -0600

    added logic to find next pages of communities

--- src/scroller-app/main.py
+++ src/scroller-app/main.py
@@ -2,10 +2,11 @@ from scrollers.edmonton_real_estate_scroller import EdmontonRealEstateScroller
 import time
 
 scroller = EdmontonRealEstateScroller()
-scroller.browse("https://www.edmontonrealestate.pro/communities.php")
+scroller.browse("https://www.edmontonrealestate.pro/central-edmonton/downtown.php")
 print("loaded")
-neighborhoods = scroller.get_communities("zone-12-13")
-for hood in neighborhoods:
-    scroller.browse(hood)
-    time.sleep(5)
-    print(hood)
+number_of_pages = scroller.get_pages()
+for i in range(1,number_of_pages):
+    scroller.get_next_page()
+    print("got page", i+1)
+    time.sleep(0.3)
+time.sleep(40)
--- src/scroller-app/scrollers/edmonton_real_estate_scroller.py
+++ src/scroller-app/scrollers/edmonton_real_estate_scroller.py
@@ -12,10 +12,27 @@ class EdmontonRealEstateScroller(WebScroller):
         self.driver.maximize_window()
     
     def get_next_page(self):
-        print("get next")
-
+        next_button = self.driver.find_element(By.XPATH, "//a[@title='Next Page']").click()
+    
     def get_previous_page(self):
-        print("get previous")
+        return super().get_previous_page()()
+    
+    def get_pages(self):
+        pagination = self.driver.find_element(By.CLASS_NAME, "pagination")
+        anchor_tags = pagination.find_elements(By.TAG_NAME, "a")
+        page_numbers = []
+        for anchor_tag in anchor_tags:
+            if len(anchor_tag.text) <= 3:
+                page_numbers.append(int(anchor_tag.text))
+        return max(page_numbers)
+    
+    def has_next_page(self):
+        try:
+            next_button = self.driver.find_elements(By.XPATH, "//a[@title='Next Page']")
+            if len(next_button) > 1:
+                return True
+        except:
+            return False
     
     def get_communities(self, zone):
         list_links = []
