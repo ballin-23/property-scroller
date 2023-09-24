832f708fd2ec15852f26bed135c71b5ba8ae50a4
ballin-23 <ballincode@gmail.com>
2023-09-24 17:48:12 -0600

    updated property class and returning property object with information

--- src/scroller-app/harvester/edmonton_real_estate_harvester.py
+++ src/scroller-app/harvester/edmonton_real_estate_harvester.py
@@ -2,6 +2,7 @@ from scrollers.web_scroller import WebScroller
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from harvester.harvester import Harvester
+from harvester.property import Property
 
 # this extracts data from a web page of properties
 class EdmontonRealEstateHarvester(Harvester):
@@ -16,11 +17,27 @@ class EdmontonRealEstateHarvester(Harvester):
 
     
     def returnPropertyInformation(self, property):
+        property = Property()
         address = property.find_element(By.CLASS_NAME, "teaser__address")
-        print(address.text)
+        property.address = address.text
         property_data = property.find_elements(By.CLASS_NAME, "teaser__additional-info")
-        for data in property_data:
-            print(data.text)
-        # print(address.text)
-        # print(property.get_attribute("href"))
+        for i in range(len(property_data)):
+            if i == 0:
+                # handle the logic for storing the type of property
+                pass
+            else:
+                # handle the logic to split text and return bedrooms, bathrooms and sqft
+                pass
+    
+    def parsePropertyInformation(self, content, property):
+        propertyInformation = content.split()
+        if len(propertyInformation) >= 5:
+            property.bedrooms = propertyInformation[0]
+            property.bathrooms = propertyInformation[2]
+            property.squarefootage = propertyInformation[4]
+        return property
+    
+    def determinePropertyType(self, description):
+        pass
+            
 
--- src/scroller-app/harvester/property.py
+++ src/scroller-app/harvester/property.py
@@ -1,6 +1,8 @@
 class Property:
-    def __init__(self, address, price, bedrooms, bathrooms):
+    def __init__(self, address, price, bedrooms, bathrooms, squarefootage, propertyType):
         self.address = address
         self.price = price
         self.bedrooms = bedrooms
         self.bathrooms = bathrooms
+        self.squarefootage = squarefootage
+        self.propertyType = propertyType
