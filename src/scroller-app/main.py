cd9d722b5e5e01b6223e8980a4fb7bcb0016b1da
ballin-23 <ballincode@gmail.com>
2023-09-24 17:18:39 -0600

    added logic to run main back

--- src/scroller-app/main.py
+++ src/scroller-app/main.py
@@ -4,18 +4,19 @@ import time
 from selenium import webdriver
 
 
-driver = webdriver.Chrome()
-scroller = EdmontonRealEstateScroller(driver)
-harvester = EdmontonRealEstateHarvester(driver)
-#TODO write code to get the link of all communities
-scroller.browse("https://www.edmontonrealestate.pro/central-edmonton/downtown.php")
-print("loaded")
-number_of_pages = scroller.get_total_pages()
-for i in range(1,number_of_pages):
-    propreties = harvester.getProperties()
-    for property in propreties:
-        harvester.returnPropertyInformation(property)
-    scroller.get_next_page()
-    print("got page", i+1)
-    time.sleep(0.5)
-time.sleep(40)
+if __name__ == "__main__":
+    driver = webdriver.Chrome()
+    scroller = EdmontonRealEstateScroller(driver)
+    harvester = EdmontonRealEstateHarvester(driver)
+    #TODO write code to get the link of all communities
+    scroller.browse("https://www.edmontonrealestate.pro/central-edmonton/downtown.php")
+    print("loaded")
+    number_of_pages = scroller.get_total_pages()
+    for i in range(1,number_of_pages):
+        propreties = harvester.getProperties()
+        for property in propreties:
+            harvester.returnPropertyInformation(property)
+        scroller.get_next_page()
+        print("got page", i+1)
+        time.sleep(0.5)
+    time.sleep(40)
